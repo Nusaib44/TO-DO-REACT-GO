4e88c0f2bbf5ad104f29314efdbb46997e54c0ab
Nusaib <muhammednusaib44@gmail.com>
2023-06-16 14:14:33 +0530

    routes added

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -1,6 +1,19 @@
 package routes
 
+import (
+	"todo/middleware"
 
-func Routes(){
-	
+	"github.com/gorilla/mux"
+)
+
+func Router() *mux.Router {
+
+	routes := mux.NewRouter()
+	routes.HandleFunc("/task", middleware.GetallTask).Methods("GET", "OPTIONS")
+	routes.HandleFunc("/tasks", middleware.Createtask).Methods("POST", "OPTIONS")
+	routes.HandleFunc("/task/{id}", middleware.Taskcomplete).Methods("PUT", "OPTIONS")
+	routes.HandleFunc("/task/undotask/{id}", middleware.Undotask).Methods("PUT", "OPTIONS")
+	routes.HandleFunc("/task/deletetask/{id}", middleware.Deletetask).Methods("DELETE", "OPTIONS")
+	routes.HandleFunc("/task/deletealltask/", middleware.Deletealltask).Methods("DELETE ", "OPTIONS")
+	return routes
 }
